450171bf5c3800d8bd32510d3523321bdf6fc56f
Anton Zavalniuk <antonzavalniuk@gmail.com>
2025-05-09 19:10:57 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ThemeSwitcher from "./components/ThemeSwitcher.vue";
+</script>
+
+<template>
+  <main class="wrapper">
+    <h1>MEMORY GAME</h1>
+    <ThemeSwitcher />
+  </main>
+</template>
+
+<style scoped>
+.wrapper {
+  position: relative;
+  display: grid;
+  place-content: center;
+  min-height: 100dvh;
+}
+</style>
